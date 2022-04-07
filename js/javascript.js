47d9a8b82b6f007bb69725821cd26d18344ad628
point671 <52425678+point671@users.noreply.github.com>
2022-04-08 00:07:36 +0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+
+payButton = document.querySelector(".pay__button_blue");
+
+
+
+payButton.addEventListener("click", function (e) {
+    loadWrap = document.querySelector(".loading");
+    loadWrap.classList.add('hide');
+
+    startTwo = document.querySelector(".two");
+    startTwo.style.display = "flex";
+});
+
+
+
+
+
+
+
